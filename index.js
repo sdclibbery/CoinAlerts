769ba1f81b477fef72e823880f80811269c866be
Stephen Clibbery <sdclibbery@googlemail.com>
2017-08-30 14:18:20 +0100

    fetch historic data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 
-fetch('https://api.coinbase.com/v2/prices/spot?currency=GBP')
+fetch('https://api.coinbase.com/v2/prices/historic?currency-pair=BTC-GBP')
     .then(function(res) {
         return res.text();
     }).then(function(body) {
